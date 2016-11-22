41e956761ebaacb1483b5e3581a1325507c043ef
Akash Thakur <skyatwork007@gmail.com>
2016-11-22 10:49:22 +0530

    Create page monitor.sh

---
+++ page monitor.sh
@@ -0,0 +1,14 @@
+url = input()
+
+wget $url -o page.html
+
+size = ls -l page.html | awk '{print $5}'
+
+limit = 1520   # you have to set the limit which decided weather to alert you or not 
+
+if [$size -gt $limit ]
+  then 
+  echo $size | ssmtp -s "test" sky.srmse@gmail.com
+fi
+
+rm -rf page.html
