0008375c48a7e85a366b12fa04f40f66a9e99394
Akash Thakur <skyatwork007@gmail.com>
2016-12-06 13:03:22 +0530

    Update page monitor.sh

--- page monitor.sh
+++ page monitor.sh
@@ -1,12 +1,10 @@
 #!/bin/bash
 
-url = input("enter the url you want to monitor")
+input("enter the url you want to monitor") | wget -o page.html
 
-wget $url -o page.html
+size=$(ls -l page.html | awk '{print $5}')
 
-size = ls -l page.html | awk '{print $5}'
-
-limit = 1520   # you have to set the limit which decided weather to alert you or not 
+$limit = 1520   # you have to set the limit which decided weather to alert you or not 
 
 if [$size -gt $limit ]
   then 
