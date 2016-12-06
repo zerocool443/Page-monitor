396726cea1e0406bda4dd72725fe2fc1f438318a
Akash Thakur <skyatwork007@gmail.com>
2016-12-06 14:02:15 +0530

    Update page monitor.sh

--- page monitor.sh
+++ page monitor.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
-input("enter the url you want to monitor") | wget -o page.html
+echo "enter the url you want to monitor"
+read url
+
+wget $url -O page.html
 
 size=$(ls -l page.html | awk '{print $5}')
 
-$limit = 1520   # you have to set the limit which decided weather to alert you or not 
+# you have to set the limit which decided weather to alert you or not 
 
-if [$size -gt $limit ]
-  then 
-  echo $size | ssmtp -s "test" yourmail@mail.com
-  rm -rf page.html
+limit=100
+
+if [ "$size" -gt "$limit" ];then
+         echo $size | ssmtp -s "test" skyatwork007@gmail.com
+         rm -rf page.html
 fi
 rm -rf page.html
-
